45c89f8957c4bf643f8eb20ea696474462b299b8
Josefin <yunalescca@gmail.com>
2017-11-11 18:43:23 +0100

    More on lab 2

--- Labs/W2/BlackJack.hs
+++ Labs/W2/BlackJack.hs
@@ -1,6 +1,7 @@
 module BlackJack where
 import Cards
 import RunGame
+import System.Random
 
 -- * Assignment 3.2
 
@@ -38,8 +39,8 @@ valueCard card = valueRank (rank card)
 numberOfAces :: Hand -> Integer
 numberOfAces Empty  = 0
 numberOfAces (Add c h) 
-    | rank c == Ace = 1 + numberOfAces h
-    | otherwise     = 0 + numberOfAces h
+    | rank c == Ace = numberOfAces h + 1
+    | otherwise     = numberOfAces h
 
 
 -- | Calculates the hand's value. If value exceeds 21 and 
@@ -68,12 +69,77 @@ gameOver (Add c h)
 -- if bank is not busted and guest has higher value than the bank
 winner :: Hand -> Hand -> Player
 winner guest bank
-    | gameOver guest == False && ((gameOver bank == True) 
-        || (gameOver bank == False && value guest > value bank)) = Guest
+    | not (gameOver guest) && ((gameOver bank 
+        || not (gameOver bank) && value guest > value bank)) = Guest
     | otherwise = Bank
 
 
+-- | Puts the first hand on top of the second hand
+(<+) :: Hand -> Hand -> Hand
+(<+) Empty hand2   = hand2
+(Add c h) <+ hand2 = Add c (h <+ hand2)
 
+
+-- | Returns the full deck (52 card)
+fullDeck :: Hand 
+fullDeck = deck Hearts <+ deck Spades <+ deck Diamonds <+ deck Clubs
+
+
+-- | Given a suit, returns all 13 cards in that suit
+deck :: Suit -> Hand
+deck suit = Add (Card (Numeric 2)  suit)
+           (Add (Card (Numeric 3)  suit)
+           (Add (Card (Numeric 4)  suit)
+           (Add (Card (Numeric 5)  suit)
+           (Add (Card (Numeric 6)  suit)
+           (Add (Card (Numeric 7)  suit)
+           (Add (Card (Numeric 8)  suit)
+           (Add (Card (Numeric 9)  suit)
+           (Add (Card (Numeric 10) suit)
+           (Add (Card Jack         suit)
+           (Add (Card Queen        suit)
+           (Add (Card King         suit)
+           (Add (Card Ace          suit) 
+            Empty))))))))))))
+
+
+-- | Given a deck and a hand, draw a card from the deck and place in the hand
+draw :: Hand -> Hand -> (Hand, Hand)
+draw Empty _           = error "draw: the deck is empty"
+draw (Add c deck) hand = (deck, (Add c hand))
+
+
+-- |
+playBank :: Hand -> Hand
+playBank deck = snd (playBank' deck Empty)
+
+
+-- |
+playBank' :: Hand -> Hand -> (Hand, Hand)
+playBank' deck bankHand 
+    | value bankHand < 16 = playBank' deck' (bankHand')
+    | otherwise = (deck', bankHand)
+
+    where (deck', bankHand') = draw deck bankHand
+
+
+-- |
+{-shuffle :: StdGen -> Hand -> Hand 
+shuffle g hand = 
+    where (card', oldHand) = removeCard oneRanomInteger g hand-}
+
+-- | 
+removeCard :: Integer -> Hand -> Hand
+removeCard _ Empty = Empty
+removeCard nthCard (Add c h)
+    | nthCard == 1 = h
+    | otherwise    = Add c (removeCard (nthCard - 1) h)
+    
+
+-- |
+oneRanomInteger :: StdGen -> Integer -> Integer
+oneRanomInteger g sizeDeck  = n1
+    where (n1, g1)          = randomR (1, sizeDeck) g
 
 
 
