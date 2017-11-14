e5aa833205657668f7aec222215bf020f5169b68
Josefin <yunalescca@gmail.com>
2017-11-14 21:14:58 +0100

    Started Lab 3

---
+++ Labs/W3/sudoku.hs
@@ -0,0 +1,91 @@
+
+import Test.QuickCheck
+
+-------------------------------------------------------------------------
+
+-- | Representation of sudoku puzzlese (allows some junk)
+data Sudoku = Sudoku { rows :: [[Maybe Int]] }
+ deriving ( Show, Eq )
+
+-- | A sample sudoku puzzle
+example :: Sudoku
+example =
+    Sudoku
+      [ [j 3,j 6,n  ,n  ,j 7,j 1,j 2,n  ,n  ]
+      , [n  ,j 5,n  ,n  ,n  ,n  ,j 1,j 8,n  ]
+      , [n  ,n  ,j 9,j 2,n  ,j 4,j 7,n  ,n  ]
+      , [n  ,n  ,n  ,n  ,j 1,j 3,n  ,j 2,j 8]
+      , [j 4,n  ,n  ,j 5,n  ,j 2,n  ,n  ,j 9]
+      , [j 2,j 7,n  ,j 4,j 6,n  ,n  ,n  ,n  ]
+      , [n  ,n  ,j 5,j 3,n  ,j 8,j 9,n  ,n  ]
+      , [n  ,j 8,j 3,n  ,n  ,n  ,n  ,j 6,n  ]
+      , [n  ,n  ,j 7,j 6,j 9,n  ,n  ,j 4,j 3]
+      ]
+  where
+    n = Nothing
+    j = Just
+
+-- * A1
+
+-- | allBlankSudoku is a sudoku with just blanks
+allBlankSudoku :: Sudoku
+allBlankSudoku = Sudoku $ replicate 9 $ replicate 9 Nothing
+
+
+-- * A2
+
+-- | isSudoku sud checks if sud is really a valid representation of a sudoku
+-- puzzle
+isSudoku :: Sudoku -> Bool
+isSudoku sudoku = 
+  correctRowSize sudoku && correctColumnSize sudoku
+
+correctRowSize :: Sudoku -> Bool
+correctRowSize sudoku = length (rows sudoku) == 9
+
+correctColumnSize :: Sudoku -> Bool
+correctColumnSize sudoku = 
+  and $ map (\x -> (length x == 9)) (rows sudoku)
+
+
+-- * A3
+
+-- | isFilled sud checks if sud is completely filled in,
+-- i.e. there are no blanks
+isFilled :: Sudoku -> Bool
+isFilled = undefined
+
+-------------------------------------------------------------------------
+
+-- * B1
+
+-- |b printSudoku sud prints a nice representation of the sudoku sud on
+-- the screen
+printSudoku :: Sudoku -> IO ()
+printSudoku = undefined
+
+-- * B2
+
+-- | readSudoku file reads from the file, and either delivers it, or stops
+-- if the file did not contain a sudoku
+readSudoku :: FilePath -> IO Sudoku
+readSudoku = undefined
+
+-------------------------------------------------------------------------
+
+-- * C1
+
+-- | cell generates an arbitrary cell in a Sudoku
+cell :: Gen (Maybe Int)
+cell = undefined
+
+
+-- * C2
+
+-- | an instance for generating Arbitrary Sudokus
+instance Arbitrary Sudoku where
+  arbitrary =
+    do rows <- vectorOf 9 (vectorOf 9 cell)
+       return (Sudoku rows)
+
+-------------------------------------------------------------------------
