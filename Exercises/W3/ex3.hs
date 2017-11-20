b180371c540ade8b72fa26101bee96e5c7bb236e
Josefin <yunalescca@gmail.com>
2017-11-20 10:22:20 +0100

    Exercises

--- Exercises/W3/ex3.hs
+++ Exercises/W3/ex3.hs
@@ -61,34 +61,104 @@ prop_zip as bs cs = zip3 as bs cs == zip3' as bs cs
 
 -----------------------------------------------------------------------------
 
--- * Permutations
+-- 1 * Permutations
 
 isPermutation :: Eq a => [a] -> [a] -> Bool
 
+-- [1,1,2] == [1,2,1] /= [1,2,2]
+isPermutation [] [] = True
+isPermutation [] bs = False
+isPermutation (a:as) bs
+    | a `elem` bs = isPermutation as (bs \\ [a])
+    | otherwise = False
 
 
+prop_permutation xs = isPermutation xs (reverse xs)
+
+-----------------------------------------------------------------------------
+
+-- 2 * Avoiding Duplicates
+
+-- Done last week
+
+-----------------------------------------------------------------------------
+
+-- 3 * Pascal's Triangle
+
+-- Should compute the n:th row of Pascal's Triangle
+pascal :: Int -> [Int]
+
+pascal 0 = [1]
+pascal n = []
+
+-----------------------------------------------------------------------------
+
+-- 4 * Erastosthenes' sieve
+
+-- removes all multiples of m from ns
+crossOut :: Int -> [Int] -> [Int]
+crossOut m ns = [n | n <- ns, n `mod` m /= 0]
+
+-- Finds all prime numbers in a list
+sieve :: [Int] -> [Int]
+sieve [] = []
+sieve (x:xs) = x : sieve (crossOut x xs)
+
+range = [2..100]
+
+-----------------------------------------------------------------------------
+
+-- 5 * Number games
+-- Test wether n is a prime (in range 2..100)
+-- Test wether n is the sum of two primes (in range 2..100)
+
+isPrime :: Int -> Bool
+isPrime n = n `elem` sieve range
+
+isSumOfPrimes :: Int -> Bool
+isSumOfPrimes n = or [(n - p) `elem` primes | p <- primes]
+    where primes = sieve range
+
+-----------------------------------------------------------------------------
+
+-- 6 * Occurrences in List
+
+-- * If an element occurs in a list
+-- With list comprehension
+occursIn :: Eq a => a -> [a] -> Bool
+occursIn x xs = or [x == y | y <- xs]
+
+-- With recursion
+occursIn2 :: Eq a => a -> [a] -> Bool
+occursIn2 _ [] = False
+occursIn2 x (y:ys)
+    | x == y    = True
+    | otherwise = occursIn2 x ys
 
 
+-- All elements in xs should be in ys
+allOccurIn :: Eq a => [a] -> [a] -> Bool
+allOccurIn xs ys = and [occursIn x ys| x <- xs]
 
 
+-- xs and ys should contain exactly the same elements
+sameElements :: Eq a => [a] -> [a] -> Bool
+sameElements xs ys = allOccurIn xs ys && allOccurIn ys xs
 
 
+-- The number of occurrences of x in xs
+numOfOccurrences :: Eq a =>  a -> [a] -> Int
+numOfOccurrences x xs = sum [1 | y <- xs, x == y]
 
+-----------------------------------------------------------------------------
 
+-- 7 * 
 
+-----------------------------------------------------------------------------
 
+-- 8 *
 
-
-
-
-
-
-
-
-
-
-
-
+-----------------------------------------------------------------------------
 
 
 
